dedeeca5e5915749b474bdc66602df14b07b776e
Mohamed-Yassir <mo.gafar25@gmail.com>
2024-02-16 11:08:39 -0800

    inventing a combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,19 +15,20 @@ int main(void)
 	for (c = '0'; c < '9'; c++)
 	{
 		for (d = c + 1; d <= '9'; d++)
-		{
-			if (d != c)
-			{
-				putchar(c);
-				putchar(d);
-				 if (c == '8' && d == '9')
-					 continue;
-				 putchar(',');
-				 putchar(' ');
-			}
-		}
+	{
+		if (d != c)
+	{
+		putchar(c);
+		putchar(d);
+	 if (c == '8' && d == '9')
+	 continue;
+		 putchar(',');
+		 putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 
 	return (0);
+
 }
