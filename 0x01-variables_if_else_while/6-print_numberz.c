2da087495e7c5a3abf103c4f6dd59e0c8dddcccd
Mohamed-Yassir <mo.gafar25@gmail.com>
2024-02-16 10:03:06 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
+
 {
 	int d;
 
-	for (d = 0; d < 10; d++)
+	for (d = '0'; d <= '9'; d++)
 		putchar(d);
 	putchar('\n');
 
