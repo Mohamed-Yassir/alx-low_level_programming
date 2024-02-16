f99c828a0ec9ea2b938eb311796a65815794a63a
Mohamed-Yassir <mo.gafar25@gmail.com>
2024-02-16 10:32:05 -0800

    hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,8 +14,9 @@ int main(void)
 	char c;
 
 	for (i = '0'; i <= '9'; i++)
-		putchar(i + '0');
-	for (c = 'a'; c < 'g'; c++)
+		putchar(i);
+
+	for (c = 'a'; c <= 'f'; c++)
 		putchar(c);
 	putchar('\n');
 
