0b73a15c887fb76845ca73b6501868da7b75c1b7
Mohamed-Yassir <mo.gafar25@gmail.com>
2024-02-16 08:57:31 -0800

    Numberss

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*
+/**
  * main - Entry point
  * Return: Always 0 (Success)
  */
@@ -12,8 +12,8 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
-	printf("%d\n", a);
+	for (a = 0; a < 10; a++)
+	printf("%d", a);
 	printf("\n");
 
 	return (0);
