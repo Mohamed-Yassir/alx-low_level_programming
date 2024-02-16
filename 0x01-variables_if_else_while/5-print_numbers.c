51c66167a307d6c06c825e3e948646bbb568a2e6
Mohamed-Yassir <mo.gafar25@gmail.com>
2024-02-16 08:47:44 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - Print all single digits of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a <= 9; a++);
+	printf("%d\n", a);
+
+	return (0);
+}
+
