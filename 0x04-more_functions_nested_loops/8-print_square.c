974d03ce531e33742702494b8d7351794073c739
Mohamed-Yassir <mo.gafar25@gmail.com>
2024-02-22 04:51:22 -0800

    sunshine

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - print a square followed by a new line
+ * @size: the number and size  of the square
+ * Return: empty
+ */
+
+void print_square(int size)
+
+{
+	int x, z;
+
+	if (size <= o)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < size; x++)
+	{
+	for (z = 0; z < size; z++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
