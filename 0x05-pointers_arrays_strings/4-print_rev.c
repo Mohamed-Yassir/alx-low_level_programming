37c8c3dd5984b3af6f20053acedb334ed1e21815
root <Mohamed-Yassir.mo.gafar25@gmail.com>
2024-09-11 06:53:18 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reversed string, followed by a new line
+ * @s: pointer to the string to print
+ * Return: void
+ */
+
+void print_rev(char *s0)
+{
+	int a = 0;
+
+	while (s[a])
+		a++;
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
